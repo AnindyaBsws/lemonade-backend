e1a484616a6a770b8945b0e1124bda775b28ce98
AnindyaBsws <shakhar004@gmail.com>
2025-04-26 22:57:18 +0530

    Error Handled

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -7,13 +7,24 @@ class ApiError extends Error{
     ){
         super(message)
         this.statusCode = statusCode
-        this.data = null
+        this.data = null 
+        /*this.data=null :::::Setting a NULL value is appropriate when the 
+        actual value is unknown,or when a value is not meaningful. A NULL 
+        value is not equivalent to a value of ZERO if the data type is a 
+        number and is not equivalent to spaces if the data type is a character. 
+        A NULL value can be inserted into columns of any data type.
+        The null value represents the intentional absence of any object value. 
+        It is one of JavaScript's primitive values and is treated as 
+        falsy for boolean operations. */
         this.message = message
         this.success = false
         this.errors = this.errors
 
         if(stack){
             this.stack = stack
+        }else{
+            Error.captureStackTrace( this, this.constructor)
         }
     }
 }
+export {ApiError}
